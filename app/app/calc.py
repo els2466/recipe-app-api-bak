0e4378095c031ca6030ec49dfa201d32460b876b
Ed Sterling <els2466@gmail.com>
2019-10-22 09:36:59 -0400

    Fixed flake8 script

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    """Add two numbers together and return the result"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract two numbers together and return the result"""
+    return y - x
